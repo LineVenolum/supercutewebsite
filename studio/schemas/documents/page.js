acc6e3fe54b02b4cb40dcb66df57468c2ca64e18
LineVenolum <57433338+LineVenolum@users.noreply.github.com>
2021-12-21 13:43:52 +0100

    Change order on page.js

--- studio/schemas/documents/page.js
+++ studio/schemas/documents/page.js
@@ -1,33 +1,25 @@
 export default {
   name: "page",
-  type: "document",
   title: "Pages",
+  type: "document",
   fields: [
     {
       name: "title",
-      type: "string",
       title: "Title",
+      type: "string",
       description: "This is the name of the site",
     },
     {
       name: "herobanner",
       title: "Herobanner",
       type: "array",
-      of: [
-        {
-          type: "herobanner",
-        },
-      ],
+      of: [{ type: "herobanner" }],
     },
     {
       name: "section",
       title: "Sections",
       type: "array",
-      of: [
-        {
-          type: "section",
-        },
-      ],
+      of: [{ type: "section" }],
     },
   ],
 };
